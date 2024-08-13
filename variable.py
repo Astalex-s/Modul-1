9a5827031e2a0e105d480e2dc34429aa4520695d
Aleksey Astafev <68611841+Astalex-s@users.noreply.github.com>
2024-08-13 06:52:10 +0500

    13/08

--- variable.py
+++ variable.py
@@ -2,5 +2,5 @@ home_work = 12
 hour = 1.5
 name_course = "Python"
 
-print(f'{name_course}, всего задач:{home_work}, затрачено часов:{hour},'
+print(f'{name_course}, всего задач:{home_work}, затрачено часов:{hour}, '
       f'среднее время выполнения {hour / home_work} часа.')
